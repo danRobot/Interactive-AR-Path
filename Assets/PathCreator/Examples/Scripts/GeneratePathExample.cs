1180c56ae966badeffc1d01c70cafdc5656afaf1
danRobot <danielgallo502@gmail.com>
2020-01-19 00:26:59 -0500

    fine

---
+++ Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PathCreation.Examples {
+    // Example of creating a path at runtime from a set of points.
+
+    [RequireComponent(typeof(PathCreator))]
+    public class GeneratePathExample : MonoBehaviour {
+
+        public bool closedLoop = true;
+        //public Transform[] waypoints;
+        public List<Transform> waypoints = new List<Transform>();
+        public Transform myPrefab;
+        BezierPath bezierPath;
+        Vector3 pointPos;
+        RaycastHit hit;
+        int i=0;
+        void Starte () {
+            if (waypoints.Count > 0) {
+                // Create a new bezier path from the waypoints.
+                 bezierPath= new BezierPath (waypoints, closedLoop, PathSpace.xyz);
+                GetComponent<PathCreator> ().bezierPath = bezierPath;
+            }
+        }
+        
+        
+        void Update(){
+            //Debug.Log(bezierPath.NumAnchorPoints);
+            //Debug.Log(i);
+            if(Input.GetMouseButtonDown(0)){
+                //Debug.Log(Input.mousePosition);
+                RaycastHit hit;
+                Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+                //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+                if (Physics.Raycast(ray, out hit,100)){
+                    pointPos = hit.point;
+                    //Debug.Log(hit.point);
+                    //transform.position = pointPos;
+                }
+                //Debug.Log(waypoints);
+                
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = pointPos;
+                //Instantiate(myPrefab, pointPos, Quaternion.identity);
+
+                waypoints.Add(Instantiate(myPrefab, pointPos, Quaternion.identity));
+                for(int j=0;j<waypoints.Count;j++){
+                    //waypoints[j].position=pointPos.position
+                    Debug.Log(waypoints[j].position);
+                }
+                //bezierPath.AddSegmentToEnd(pointPos);
+                //Debug.Log(bezierPath.NumAnchorPoints);
+                //closedLoop=!closedLoop;
+                bezierPath=new BezierPath (waypoints,closedLoop,PathSpace.xyz);
+                //GetComponent<PathCreator> ().bezierPath = bezierPath;
+                //bezierPath.MovePoint(0,pointPos);
+                i++;
+                GetComponent<PathCreator> ().bezierPath = bezierPath;
+            }
+            if (Input.GetMouseButtonDown(1)){
+                
+                bezierPath.MovePoint(0,pointPos);}
+            if (Input.GetMouseButtonDown(2))
+                Debug.Log("Pressed middle click.");
+        }
+        
+        void Start(){
+            pointPos=new Vector3(0.0f, 0.0f, 0.0f);
+            //GameObject plane  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            Starte();
+        }
+    }
+}
